37c335f382e2788fe15f526d28a2bfe161e4c010
Suziane Cundiff <svlc@icomp.ufam.edu.br>
2020-04-07 16:17:36 -0400

    Update
    Using the same .gitignore than Brackets.

---
+++ joguinho2D/Assets/Scripts/player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public bool isJumping;
+    public bool doubleJump;
+    private Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump")) //isJumping precisa ser falso.
+        {
+            if(!isJumping)
+            {
+                rigid.AddForce(new Vector2(0f,jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    rigid.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8) // layer Ground
+        {
+            isJumping = false;
+        }    
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8) // layer Ground
+        {
+            isJumping = true;
+        }  
+        
+    }
+}
